12020df28c2d7cea77cacc6626f65a05e2a76355
Roger Ineichen <roger@projekt01.ch>
2007-12-29 01:56:51 +0000

    Added dev marker to version number

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.layer.ready2go',
-    version='0.5.0',
+    version='0.5.0dev',
     author = "Stephan Richter, Roger Ineichen and the Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "A ready to go layer for Zope3",
