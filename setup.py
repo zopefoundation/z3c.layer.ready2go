50c083d8efe724226de065cb8c2ac8c979c63c34
Michael Howitz <mh@gocept.com>
2021-01-08 16:43:20 +0100

    Update setup.py
    Co-authored-by: Marius Gedminas <marius@gedmin.as>

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
         'Operating System :: OS Independent',
         'Topic :: Internet :: WWW/HTTP',
         'Framework :: Zope :: 3'],
-    url='http://pypi.python.org/pypi/z3c.layer.ready2go',
+    url='https://github.com/zopefoundation/z3c.layer.ready2go',
     packages=find_packages('src'),
     include_package_data=True,
     package_dir={'': 'src'},
