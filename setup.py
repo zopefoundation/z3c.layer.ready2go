cc969eb5162cd6ef5d3b7aa525ed305f51f6afcd
Michael Howitz <icemac@gmx.net>
2025-03-27 07:51:59 +0100

    Add needed changes.

--- setup.py
+++ setup.py
@@ -70,7 +70,6 @@ setup(
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Topic :: Internet :: WWW/HTTP',
